26fe4963f7a8bce5fe6cee418f20494875b7d190
joneseyit <joneseyit@protonmail.com>
2018-10-02 23:44:08 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,4 @@
 function forLoop(array){
   for(let i = 0; i < 25; i++){
-      if(i === 0) {
-        array.push("I am ${i} strange loop.")
-    } else {
-      array.push("I am ${i} strange loops.")
-    }
-  }
+   array.push("I am ${i} strange loop${i === 0? '': 's'" )
 }
