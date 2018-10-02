01570171ca1eb8f1401e40c289e35329a377aba1
joneseyit <joneseyit@protonmail.com>
2018-10-02 23:39:35 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(xRay){
   for(i = 0; i < 25; i++){
-      if(i === 0) {
+      if(i === 1) {
         xRay.push("I am 1 strange loop.")
     } else {
       xRay.push("i am ${i} strange loops.")
